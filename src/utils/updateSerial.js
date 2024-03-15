a15df4ad9544124500792976edacaeb47a06799a
Nauman Ahmed <35535536+nauman-ahmed@users.noreply.github.com>
2024-03-15 16:38:02 +0100

    added chrome

--- src/utils/updateSerial.js
+++ src/utils/updateSerial.js
@@ -9,16 +9,7 @@ const UpdateSerialNumber = require("../utils/updateSerial")
 
 async function convertHtmlToImage(htmlContent, outputFile,width) {
     const browser = await puppeteer.launch({
-      args: [
-        "--disable-setuid-sandbox",
-        "--no-sandbox",
-        "--single-process",
-        "--no-zygote",
-      ],
-      executablePath:
-        process.env.NODE_ENV === "production"
-          ? process.env.PUPPETEER_EXECUTABLE_PATH
-          : puppeteer.executablePath(),
+      executablePath: '/usr/bin/chromium-browser'
     });
     const page = await browser.newPage();
   
